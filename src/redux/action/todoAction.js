84e695e32cdc60e87a6b516f909641774678bb0a
unknown <jehan.adhan01@gmail.com>
2023-03-01 20:22:31 +0700

    embuh_wes

--- src/redux/action/todoAction.js
+++ src/redux/action/todoAction.js
@@ -1,25 +1,40 @@
 import { Axios } from "axios";
 
+// export const getData = () => {
+//   // sebuah function
+//   return (dispatch) => {
+//     //me-return function
+//     Axios.get("http://localhost:2000/activities").then((res) => {
+//       dispatch({
+//         //yg me-return object dan di kirim ke reducer
+//         type: "GET_DATA",
+//         payload: res.data,
+//       });
+//     });
+//   };
+// };
+
 export const getData = () => {
-  // sebuah function
-  return (dispatch) => {
-    //me-return function
-    Axios.get("http://localhost:2000/activities").this((rest) => {
+  return async (dispatch) => {
+    try {
+      const res = await Axios.get("http://localhost:2000/activities");
       dispatch({
-        //yg me-return object dan di kirim ke reducer
         type: "GET_DATA",
-        payload: rest.data,
+        payload: res.data,
       });
-    });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
+
 export const addData = (obj) => {
   return (dispatch) => {
-    Axios.post("http://localhost:2000/activities", obj).then((rest) => {
-      Axios.get("http://localhost:2000/activities").then((rest) => {
+    Axios.post("http://localhost:2000/activities", obj).then((res) => {
+      Axios.get("http://localhost:2000/activities").then((res) => {
         dispatch({
           type: "GET_DATA",
-          payload: rest.data,
+          payload: res.data,
         });
       });
     });
@@ -27,11 +42,11 @@ export const addData = (obj) => {
 };
 export const delData = (id) => {
   return (dispatch) => {
-    Axios.delete(`http://localhost:2000/activities/${id}`).then((rest) => {
-      Axios.get("http://localhost:2000/activities").then((rest) => {
+    Axios.delete(`http://localhost:2000/activities/${id}`).then((res) => {
+      Axios.get("http://localhost:2000/activities").then((res) => {
         dispatch({
           type: "GET_DATA",
-          payload: rest.data,
+          payload: res.data,
         });
       });
     });
@@ -41,11 +56,11 @@ export const Complete = (id) => {
   return (dispatch) => {
     Axios.patch(`http://localhost:2000/activities/${id}`, {
       isComplete: true,
-    }).then((rest) => {
-      Axios.get("http://localhost:2000/activities").then((rest) => {
+    }).then((res) => {
+      Axios.get("http://localhost:2000/activities").then((res) => {
         dispatch({
           type: "GET_DATA",
-          payload: rest.data,
+          payload: res.data,
         });
       });
     });
